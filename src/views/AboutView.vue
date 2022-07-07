ef9a45d0f583466b2ed679122d0821475a91e776
John Knotts <jhnnyk@gmail.com>
2022-07-07 15:58:03 -0600

    router links and strip default content

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,38 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Est aliquid quam,
+      in quaerat dolore cum quas rerum perferendis repellat pariatur!
+      Consequuntur, ab tenetur dolorum cupiditate minima eligendi quasi iusto
+      deserunt perspiciatis neque. Maxime voluptates minima mollitia.
+      Accusantium cumque dolores facere obcaecati quibusdam, velit at sunt,
+      repellendus, id vitae architecto enim illo aspernatur eius? Asperiores et
+      maxime voluptates delectus, quisquam dolores culpa doloremque velit, illum
+      possimus aperiam laboriosam nobis, veritatis eius necessitatibus
+      aspernatur sit sed distinctio perspiciatis iste beatae quaerat optio! Sunt
+      consequuntur cupiditate, fuga dolores quasi, ut nostrum ipsa debitis in
+      quia inventore delectus eligendi dignissimos doloremque, consequatur
+      maxime obcaecati.
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque aperiam
+      autem eius quae ipsum sequi maxime, quo harum totam doloremque, aspernatur
+      reprehenderit vitae animi voluptas minima alias officia. Suscipit veniam
+      optio molestiae. Rem aspernatur enim deserunt illo excepturi repellat sint
+      perferendis eaque sequi maxime qui alias, expedita culpa, at quis eius
+      voluptatibus repudiandae ab non assumenda perspiciatis consectetur
+      consequuntur. Quasi doloremque, expedita quisquam laborum quod, a fugiat
+      eveniet amet magni fuga vel reiciendis ipsam assumenda architecto ad qui
+      iusto saepe esse explicabo possimus repudiandae dolore sed ipsa
+      laboriosam. Temporibus eaque quae aspernatur molestiae aut nam, doloribus
+      eius quos minima aperiam.
+    </p>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<script>
+export default {
+  name: 'AboutView',
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,37 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem illum,
+      dolor, quis quae eius in vel deleniti maxime impedit asperiores atque
+      voluptates blanditiis facere earum magni assumenda. Similique, sed fugit
+      aperiam aspernatur, eligendi dicta nihil distinctio culpa, voluptates
+      expedita ratione excepturi. Vitae deleniti repellendus, odio quae quisquam
+      laudantium consequatur ad, autem amet corrupti tempore, commodi ullam
+      distinctio doloribus culpa? Ipsa, ullam. Molestias, accusamus maiores et
+      est odit fugit quae. Fugiat repudiandae, asperiores a, ullam suscipit
+      officiis corporis eligendi pariatur perspiciatis distinctio dolores
+      excepturi optio quo dignissimos, magni illo sapiente molestias ut eveniet
+      repellat dolor eum. Dicta ipsam reprehenderit voluptate pariatur?
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi sint ullam
+      rem quia eveniet. Autem sed modi, mollitia expedita ipsum, rem
+      voluptatibus et odit magnam aliquid ut harum! Ullam dolorum asperiores
+      animi ab sunt consequatur praesentium, repellendus accusamus, voluptate
+      molestias dicta nam deserunt at blanditiis optio pariatur. Impedit dolorem
+      perspiciatis accusamus et hic corrupti nulla reiciendis? Expedita quos
+      sunt ipsam ipsum assumenda, quas ducimus perferendis illo dolore debitis
+      sequi nisi ad quis sit eligendi unde qui cum doloremque nostrum, nobis
+      magni pariatur aut maxime quaerat. Quos consequatur maxime eaque tempora
+      fugiat molestias quaerat officiis suscipit, ipsum ea magni accusamus
+      provident.
+    </p>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'HomeView',
+};
+</script>

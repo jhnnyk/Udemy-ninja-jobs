4204a2789ed14824adfcf7333823f965ae1bb8a5
John Knotts <jhnnyk@gmail.com>
2022-07-08 08:06:09 -0600

    folder structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,27 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import AboutView from '../views/AboutView.vue';
+import JobsView from '../views/jobs/JobsView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'Home',
+      component: HomeView,
     },
     {
       path: '/about',
-      name: 'about',
-      component: AboutView
-    }
-  ]
-})
+      name: 'About',
+      component: AboutView,
+    },
+    {
+      path: '/jobs',
+      name: 'Jobs',
+      component: JobsView,
+    },
+  ],
+});
 
-export default router
+export default router;

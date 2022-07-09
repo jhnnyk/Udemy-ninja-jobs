559f80dab3571bdffd59e99e357a034ee81ae2f9
John Knotts <jhnnyk@gmail.com>
2022-07-09 12:09:46 -0600

    route parameters

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import AboutView from '../views/AboutView.vue';
 import JobsView from '../views/jobs/JobsView.vue';
+import JobDetails from '../views/jobs/JobDetails.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/jobs',
       name: 'Jobs',
       component: JobsView,
+    },
+    {
+      path: '/jobs/:id',
+      name: 'JobDetails',
+      component: JobDetails,
     },
   ],
 });
